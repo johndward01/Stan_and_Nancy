c172f6563bcf80a43f05feeacafe35a157464700
johndward01 <johndward01@gmail.com>
2022-09-06 12:14:03 -0500

    update

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
+function myFunc() {
+  console.log("Hello");
+}
+
 let button = document.getElementById("test");
 let counter = document.getElementById("counter");
 
 button.addEventListener("click", myFunc);
-
-function myFunc() {
-  console.log("Hi");
-  counter.innerText = "10";
-}
